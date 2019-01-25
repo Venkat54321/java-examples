574d30fa934e1147dbae7ec9b28415e54b474eb6
Venkateswararao Yeluru <venkateswararao.y@appnomic.com>
2019-01-25 16:02:40 +0530

    java script.

---
+++ src/javascript/Test.java
@@ -0,0 +1,14 @@
+package javascript;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        // best book to learn from JAVA to java script.
+
+        //https://github.com/Javis1205/T-i-li-u-javascript/blob/master/The%20Principles%20of%20Object-Oriented%20JavaScript.pdf
+
+
+    }
+}
