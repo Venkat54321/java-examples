d2d1f007cda1cc7b1682d7adfe654bb6dc13dd17
Kn <kns@cns-inc.com>
2024-04-23 16:08:16 +0530

    sum of 2 numbers

---
+++ src/supriya/Sum.java
@@ -0,0 +1,26 @@
+package supriya;
+
+public class Sum {
+	public static void main(String args[]) {
+		
+		int a=1;
+		int b=2;
+		int sum;
+		int mul;
+		int sub;
+		long div;
+		
+		sum=a+b;
+		sub=b-a;
+		mul=a*b;
+		div=a/b;
+		System.out.println("sum is:"+sum);
+		System.out.println("sub is:"+sub);
+		System.out.println("mul is:"+mul);
+		System.out.println("div is:"+div);
+		
+		
+		
+	}
+
+}
