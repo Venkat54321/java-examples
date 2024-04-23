00e1281d7492ca58ded224acdbf3884cf0a85a3f
Kn <kns@cns-inc.com>
2024-04-23 18:19:24 +0530

    subtraction of 2 numbers

---
+++ src/supriyamul/Multiplication.java
@@ -0,0 +1,14 @@
+package supriyamul;
+
+public class Multiplication {
+
+	public static void main(String args[]) {
+		
+		int a=1;
+		int b=2;
+		int mul;
+		mul=a*b;
+		System.out.println("mul is:"+mul);
+		
+}
+}
